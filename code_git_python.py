fddfa599d79ac25939aeb61f0b8847b06e79e29f
David S <54046607+santiago-vas@users.noreply.github.com>
2021-12-14 18:32:38 -0500

    Update code_git_python.py

--- code_git_python.py
+++ code_git_python.py
@@ -1,17 +1,26 @@
+!pip install GitPython
+
+
 from git import Repo,remote
+import git
+import os
+import pandas as pd
+username = "username_for_github"
+password = "password"
+
+repo = git.Repo.clone_from(f'https://{username}:{password}@github.com/_git/NAMEOF THE REPOSITORY',
+                           os.path.join('name_how_you_want_to_save_it_localy'))
+
+print('completo')
+
+# agregar archivos al repositorio
+texto = pd.read_csv('reporte1.csv')
+texto.to_csv('/content/name_how_you_want_to_save_it_localy/folder1/folder2/prueba_Python.json')
 
 
-import sys
-sys.path.append('/usr/local/airflow/dags')
-rw_dir = 'archivo.json'
-repo = Repo()
-
-'''Enter code to commit the repository here.
-After commit run the following code to push the commit to remote repo.
-I am pushing to master branch here'''
-
-git_push_automation()
-
-
-origin = repo.remote(name='origin')
+repo.git.add("/content/name_how_you_want_to_save_it_localy/folder1/folder2/")
+repo.index.commit("Some commit message")
+origin = repo.remote(name="origin")
 origin.push()
+# origin.pull()
+
